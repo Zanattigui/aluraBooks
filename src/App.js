8f67202bd19ed45fa30313dbdc34d17fb7d6c463
Guilherme Zanatti <zanattigui@gmail.com>
2025-04-07 16:26:18 -0300

    Ajustando pastas

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import Header from './componentes/header'
+import styled from 'styled-components'
+
+const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589);
+`
+
+
+function App() {
+  return (
+    <AppContainer>
+      <Header/>
+    </AppContainer> //comentarioa
+  );
+}
+
+export default App;
